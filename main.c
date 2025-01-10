7910369e5446af5d76536e39ecccdceb89b65722
mmolyneaux <m_molyneaux@hotmail.com>
2025-01-09 23:14:53 -0700

    working version

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define CT_SIZE 1024 // Bitmap color table size
+#define HEADER_SIZE 54
+
 int main(int argc, char *argv[]) {
+
     FILE *streamIn = fopen("images/cameraman.bmp", "rb");
     FILE *streamOut = fopen("images/cameraman_copy.bmp", "wb");
 
@@ -9,10 +15,10 @@ int main(int argc, char *argv[]) {
         printf("Error opening file!\n");
     }
 
-    unsigned char header[54];
-    unsigned char colorTable[1024];
+    unsigned char header[HEADER_SIZE];
+    unsigned char colorTable[CT_SIZE];
 
-    for (int i = 0; i < 54; i++) {
+    for (int i = 0; i < HEADER_SIZE; i++) {
         header[i] = getc(streamIn);
     }
 
@@ -20,10 +26,38 @@ int main(int argc, char *argv[]) {
     // so it can be dereferenced into an int, so it is cast to a 4 byte int
     // instead stead of a single byte from the char header array. Then the
     // height can be retreived from the next 4 byts and so on.
-    int width = *(int *)&header[18];
-    int height = *(int *)&header[22];
-    int bitDepth = *(int *)&header[28];
+    unsigned int width = *(int *)&header[18];
+    unsigned int height = *(int *)&header[22];
+    unsigned int bitDepth = *(int *)&header[28];
 
+    const unsigned int IMAGE_SIZE = width * height;
+
+    // if the bit depth is less than or equal to 8 then we need to read the
+    // color table. The read content is going to be stored in colorTable. Not
+    // all bitmap images have color tables.    
+    bool CT_EXISTS = false;
+    if (bitDepth <= 8) {
+        CT_EXISTS = true;
+    }
+
+    if (CT_EXISTS) {
+        fread(colorTable, sizeof(unsigned char), CT_SIZE, streamIn);
+    }
+
+    fwrite(header, sizeof(unsigned char), HEADER_SIZE, streamOut);
+
+    unsigned char buffer[IMAGE_SIZE];
+
+    fread(buffer, sizeof(unsigned char), IMAGE_SIZE, streamIn);
+
+    if (CT_EXISTS) {
+        fwrite(colorTable, sizeof(unsigned char), CT_SIZE, streamOut);
+    }
+    fwrite(buffer, sizeof(unsigned char), IMAGE_SIZE, streamOut);
+    fclose(streamOut);
+    fclose(streamIn);
+    
+    
     printf("width: %d\n", width);
     printf("height: %d\n", height);
     printf("bitDepth: %d\n", bitDepth);
